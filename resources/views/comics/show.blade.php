9fcfd4cd395e77aeab66fc773723a2a693703209
Francesca <fracassani@gmail.com>
2021-01-07 18:06:37 +0100

    Controllo dettagli prodotti

--- resources/views/comics/show.blade.php
+++ resources/views/comics/show.blade.php
@@ -4,14 +4,14 @@
 @section('content')
 
     <main class="comic-detail"> 
-        <section class="general-hero" style="background-image: url( {{$comic['image-hero'] }});">
-            <div class="container">
+        <section class="hero" style="background-image: url( {{$comic['image-hero'] }});">
+            <div class="wrapper">
                 <img src="{{ $comic['image-cover'] }}" alt="{{ $comic['title'] }}">
             </div>
         </section>
 
         <section class="details mt-3 mb-3">
-            <div class="container">
+            <div class="wrapper">
                 <h1> {{ $comic['title'] }} </h1>
                 <div class="price"> {{ $comic['price'] }} </div>
                 <div class="text"> {!! $comic['body'] !!} </div>
